bddb01353a08f572ae08605f0b9fba4f27aba14b
Ghare <mghare@cci0acgq18aws.dyn.uncc.edu>
2019-06-04 17:34:58 -0400

    initial commit

--- geoFencingApp/ViewController.swift
+++ geoFencingApp/ViewController.swift
@@ -7,14 +7,66 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
+import UserNotifications
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager:CLLocationManager = CLLocationManager()
+    
+    let center = UNUserNotificationCenter.current()
+    let content = UNMutableNotificationContent()
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.distanceFilter = 100
+        
+        center.requestAuthorization(options: [.alert, .sound])
+            {(granted,error) in }
+        
+         let geoFenceRegion:CLCircularRegion = CLCircularRegion(center: CLLocationCoordinate2DMake(35.2271,-80.8431), radius: 100, identifier: "Charlotte")
+      
+        content.title = "Location Update"
+        content.body = "Exited \(geoFenceRegion.identifier)"
+        
+       
+        
+       
+        
+        locationManager.startMonitoring(for: geoFenceRegion)
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+       
+        
+        self.mapView.centerCoordinate = locations[0].coordinate
+        let reg = MKCoordinateRegion(center: locations[0].coordinate, latitudinalMeters: 8000, longitudinalMeters: 8000)
+        self.mapView.setRegion(reg, animated: true)
+        self.mapView.showsUserLocation = true
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("Entered \(region.identifier)")
+    }
 
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("Exited \(region.identifier)")
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "timerDone", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
 
